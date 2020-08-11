66f2b4ad148e49ba47e9c83363fa9d9cf84697a5
PSYmoom <saadsymoom@gmail.com>
2020-08-11 11:34:46 -0400

    Updates

---
+++ precache-manifest.99cbdb5e895d5e8e63f159fa6719a611.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "79c185a0d22c0515d5e5dd6869971828",
+    "url": "/react-portfolio/index.html"
+  },
+  {
+    "revision": "4772329b1a7e63768fdc",
+    "url": "/react-portfolio/static/css/2.ac1ab58b.chunk.css"
+  },
+  {
+    "revision": "4772329b1a7e63768fdc",
+    "url": "/react-portfolio/static/js/2.ea46fe21.chunk.js"
+  },
+  {
+    "revision": "3648b384e94d44c82691144a3d926d4d",
+    "url": "/react-portfolio/static/js/2.ea46fe21.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e0ee51244ad0849e4b81",
+    "url": "/react-portfolio/static/js/main.485b3c43.chunk.js"
+  },
+  {
+    "revision": "aff33cef9dbb8787d6b3",
+    "url": "/react-portfolio/static/js/runtime-main.213b1880.js"
+  },
+  {
+    "revision": "193fd0ab79c4ed1ab67460ec5adfd86c",
+    "url": "/react-portfolio/static/media/CV.193fd0ab.pdf"
+  },
+  {
+    "revision": "11f955cf1ab930a79c8a6d34a883a8a4",
+    "url": "/react-portfolio/static/media/bkimg.11f955cf.jpg"
+  },
+  {
+    "revision": "6b4456453c7cef0cc8be1dc34d5597f8",
+    "url": "/react-portfolio/static/media/project.6b445645.png"
+  }
+]);
